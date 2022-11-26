0e7e35a950058f0dd98fbb7759272671d80fa9a3
Noman Faisal <Noman.Faisal@avanza.pk>
2022-11-26 21:43:50 +0500

    first commit

---
+++ app/src/main/java/com/mindorks/example/android_dagger2_example/di/component/ApplicationComponent.java
@@ -0,0 +1,37 @@
+package com.mindorks.example.android_dagger2_example.di.component;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.mindorks.example.android_dagger2_example.DemoApplication;
+import com.mindorks.example.android_dagger2_example.data.DataManager;
+import com.mindorks.example.android_dagger2_example.data.DbHelper;
+import com.mindorks.example.android_dagger2_example.data.SharedPrefsHelper;
+import com.mindorks.example.android_dagger2_example.di.ApplicationContext;
+import com.mindorks.example.android_dagger2_example.di.module.ApplicationModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+
+
+
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+
+    void inject(DemoApplication demoApplication);
+
+    @ApplicationContext
+    Context getContext();
+
+    Application getApplication();
+
+    DataManager getDataManager();
+
+    SharedPrefsHelper getPreferenceHelper();
+
+    DbHelper getDbHelper();
+
+}
---
+++ app/src/main/java/com/mindorks/example/android_dagger2_example/di/module/ApplicationModule.java
@@ -0,0 +1,51 @@
+package com.mindorks.example.android_dagger2_example.di.module;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.mindorks.example.android_dagger2_example.di.ApplicationContext;
+import com.mindorks.example.android_dagger2_example.di.DatabaseInfo;
+
+import dagger.Module;
+import dagger.Provides;
+
+
+@Module
+public class ApplicationModule {
+
+    private final Application mApplication;
+
+    public ApplicationModule(Application app) {
+        mApplication = app;
+    }
+
+    @Provides
+    @ApplicationContext
+    Context provideContext() {
+        return mApplication;
+    }
+
+    @Provides
+    Application provideApplication() {
+        return mApplication;
+    }
+
+    @Provides
+    @DatabaseInfo
+    String provideDatabaseName() {
+        return "demo-dagger.db";
+    }
+
+    @Provides
+    @DatabaseInfo
+    Integer provideDatabaseVersion() {
+        return 2;
+    }
+
+    @Provides
+    SharedPreferences provideSharedPrefs() {
+        return mApplication.getSharedPreferences("demo-prefs", Context.MODE_PRIVATE);
+    }
+}
